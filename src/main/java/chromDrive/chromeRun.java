a2f533012a509bba0e85a0f2f4e5c5bdb961893c
Gurami Ivanidze <72315760+gurglosa@users.noreply.github.com>
2021-10-31 21:02:21 +0400

    updated tests
    add some test and variables, plugins

--- src/main/java/chromDrive/chromeRun.java
+++ src/main/java/chromDrive/chromeRun.java
@@ -7,6 +7,11 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class chromeRun {
+    int NegativeNumber = -10;
+    int PositiveNumber = 10;
+    String emptyString = "";
+    String NameString = "Tname";
+    String gibberish = "Lorem Ipsum";
     @BeforeTest
     public static void start(){
         WebDriverManager.chromedriver().setup();
--- src/test/java/testMainPage.java
+++ src/test/java/testMainPage.java
@@ -1,4 +1,5 @@
 import chromDrive.chromeRun;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
@@ -11,10 +12,27 @@ import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Selenide.sleep;
 public class testMainPage extends chromeRun {
+
+    @Test
+    public static void checkdefaultValues(){
+
+        Assert.assertEquals($("#Area").getValue(),"1");
+        Assert.assertEquals($("#Perimeter").getValue(),"0");
+        Assert.assertEquals($("#Angle").getValue(),"4");
+    }
     @Test
     public static void  positivtestCalculator(){
         $("#Material").selectOption("რუსული");
-        sleep(5000);
+        $("#Area").setValue("10");
+        $("#Perimeter").setValue("10");
+        $("#Angle").setValue("10");
+        $("#Lamp").setValue("10");
+        $("#Chandelier1").setValue("10");
+        $("#Chandelier2").setValue("10");
+        $("#Oval").setValue("10");
+        $("#Cant").setValue("10");
+        $("#Sum").click();
+        Assert.assertEquals($("#Sum").getText(),"1087");
     }
 
 }
